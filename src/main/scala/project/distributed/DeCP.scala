0b1d34ddea6372b22220117b526de6a49538fec5
Jeppeks1 <Jeppeks1@hotmail.com>
2018-09-15 19:42:04 +0200

    Added some code to test the DeCP Java implementation.

--- src/main/scala/project/distributed/DeCP.scala
+++ src/main/scala/project/distributed/DeCP.scala
@@ -8,20 +8,50 @@ import org.apache.flink.api.scala._
 
 import project.distributed.reader.FeatureVector._
 import project.distributed.container.Point
+import project.distributed.container.Cluster
 
+import project.distributed.container.eCPALTree
+import project.distributed.container.SiftDescriptorContainer
+import project.distributed.container.SiftKnnContainer
+import scala.math.{ceil, floor}
+import scala.util.Random
 
 object DeCP {
 
+  /**
+    *
+    * Usage:
+    * {{{
+    *   DeCP --featureVector <path> --L <Int> --a <Int>
+    * }}}
+    *
+    */
   def main(args: Array[String]): Unit = {
-    // checking input parameters
+    // Initialize input, environment and data points
+
     val params: ParameterTool = ParameterTool.fromArgs(args)
-
-    // set up execution environment
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    val points: Vector[Point] = readFeatureVector(params)
 
-    // get input data:
-    val points: DataSet[Point] = readFeatureVector(params, env)
+
+    // For testing
+    var idx = 0
+    var container = Array[SiftDescriptorContainer]()
+    for (el <- points){
+      container = container :+ new SiftDescriptorContainer(idx, el.descriptor.map(_.toByte).toArray)
+      idx = idx + 1
+    }
+
+
+    val tree = new eCPALTree
+    tree.buildIndexTreeFromLeafs(2,1,container)
+    val test = tree.getTopStaticTreeCluster(container(18), 8) // Get 8-nn to query point with id = 18
+
+
+
   }
+
+
 
 
 
