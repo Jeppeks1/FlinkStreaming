d2415ce169a9d323fa28a686bef0736fb5d062be
Jeppeks1 <Jeppeks1@hotmail.com>
2018-09-18 22:35:20 +0200

    Modified the FeatureVector class to return datasets of Points instead of Vectors.

--- src/main/scala/project/distributed/reader/FeatureVector.scala
+++ src/main/scala/project/distributed/reader/FeatureVector.scala
@@ -6,7 +6,7 @@ import java.nio.{ByteBuffer, ByteOrder}
 import org.apache.flink.api.scala._
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.commons.io.FilenameUtils
-import project.local.container.Point
+import project.distributed.container.Point
 import org.apache.flink.api.common.functions._
 
 /**
@@ -21,12 +21,34 @@ object FeatureVector {
     * The dimension of the input vectors must be d = 128 and each vector
     * must have size 4 + 4 * d.
     *
-    * @param params ParameterTool containing --featureVector pathToFile
     * @param env The current execution environment.
+    * @param params ParameterTool containing --featureVector pathToFile
     * @return A vector representation of all the points in the input file.
     */
-  def readFeatureVector(params: ParameterTool, env: ExecutionEnvironment): DataSet[Point] = {
+  def readFeatureVector(env: ExecutionEnvironment, params: ParameterTool): DataSet[Point] = {
     val path = params.get("featureVector")
+    val points = FilenameUtils.getExtension(path) match {
+      case "fvecs" => fi_vecs(path, env)
+      case "ivecs" => fi_vecs(path, env)
+      case _ => throw new IOException("Error: Unsupported data input format in readFeatureVector:\n" +
+        "Supported file extensions are .fvecs and .ivecs.")
+    }
+    points
+  }
+
+
+  /**
+    * Method for reading feature vector files with the .ivecs and .fvecs extensions.
+    * The integers are stored as floating point numbers for simplicity.
+    * The dimension of the input vectors must be d = 128 and each vector
+    * must have size 4 + 4 * d.
+    *
+    * @param env The current execution environment.
+    * @param params ParameterTool containing --queryPoint pathToFile
+    * @return A vector representation of all the points in the input file.
+    */
+  def readQueryPoints(env: ExecutionEnvironment, params: ParameterTool): DataSet[Point] = {
+    val path = params.get("queryPoint")
     val points = FilenameUtils.getExtension(path) match {
       case "fvecs" => fi_vecs(path, env)
       case "ivecs" => fi_vecs(path, env)
@@ -75,11 +97,16 @@ object FeatureVector {
         else if (ext == "fvecs")
           vec = vec :+ buffer.getFloat
       }
+      // TODO: Fix stack safety issue
       fvecs = fvecs.union(env.fromElements(Point(id, vec)))
       id = id + 1
     }
+
     fvecs.filter(_.pointID != -1)
   }
+
+
+
 }
 
 
