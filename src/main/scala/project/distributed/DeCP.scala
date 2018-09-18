f88234d32fc3081889d289edf292a4c40d60c3b5
Jeppeks1 <Jeppeks1@hotmail.com>
2018-09-18 22:36:05 +0200

    Updated the structure of the Main method to reflect the DeCP pipeline.

--- src/main/scala/project/distributed/DeCP.scala
+++ src/main/scala/project/distributed/DeCP.scala
@@ -1,53 +1,97 @@
 package project.distributed
 
 import org.apache.flink.api.java.functions.FunctionAnnotation.ForwardedFields
-import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.configuration.Configuration
-import org.apache.flink.api.common.functions._
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala._
-import project.local.reader.FeatureVector._
-import project.local.container.Point
-import project.java.{SiftDescriptorContainer, SiftKnnContainer, eCPALTree}
-import project.local.container.Cluster
 
-import scala.math.{ceil, floor}
+import project.distributed.reader.FeatureVector._
+import project.distributed.container.InternalNode
+import project.distributed.container.IndexTree._
+import project.distributed.container.Cluster
+import project.distributed.container.Point
+
+import scala.collection.JavaConverters._
 import scala.util.Random
 
 object DeCP {
+
+  import org.apache.flink.api.common.functions.{MapFunction, RichFlatMapFunction}
+
 
   /**
     *
     * Usage:
     * {{{
-    *   DeCP --featureVector <path> --L <Int> --a <Int>
+    *   DeCP --method <String>
+    *        --featureVector <path>
+    *        --queryPoint <path>
+    *        --L <Int>
+    *        --a <Int>
+    *        --b <Int>
+    *        --k <Int>
     * }}}
     *
     */
   def main(args: Array[String]): Unit = {
-    import project.local.container.Point
-    // Initialize input, environment and data points
-
+    // Initialize input and variables
     val params: ParameterTool = ParameterTool.fromArgs(args)
+    val method = params.get("method")
+    val k = params.get("k").toInt
+
+    // Get the execution environment and read the data
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
-    val points: Vector[Point] = readFeatureVector(params)
+    val queryPoints: DataSet[Point] = readQueryPoints(env, params)
+    val points: DataSet[Point] = readFeatureVector(env, params) // TODO: Broadcast?
+    val root: InternalNode = buildIndexTree(points, params) // TODO: Broadcast?
 
 
-    // For testing
-    var idx = 0
-    var container = Array[SiftDescriptorContainer]()
-    for (el <- points){
-      container = container :+ new SiftDescriptorContainer(idx, el.descriptor.map(_.toByte).toArray)
-      idx = idx + 1
+    // Process the query points
+    if (method == "scan"){
+
+//      queryPoints.first(4)
+//        .map{new KNNScanRich}
+//        .withBroadcastSet(points, "pointsIn")
+//        .print
+
+
+
+
+
+
+
     }
+    else if (method == "index"){
+
+      val clusteredPoints = points
+        .map(p => (searchIndex(root)(p).toCluster, p))
+        .withForwardedFields("_1->_2") // Input is copied unchanged to position 2 of the output
+        .groupBy(0)
 
 
-    val tree = new eCPALTree
-    tree.buildIndexTreeFromLeafs(2,1,container)
-    val test = tree.getTopStaticTreeCluster(container(18), 8) // Get 8-nn to query point with id = 18
 
 
-
+    }
   }
+
+  @ForwardedFields(Array("*->_1"))
+  final class KNearestNeighborRich extends RichMapFunction[Point, (Point, Vector[Point])]{
+    private var pointsIn: Traversable[Point] = _
+
+    override def open(parameters: Configuration): Unit =
+      pointsIn = getRuntimeContext.getBroadcastVariable[Point]("pointsIn").asScala
+
+
+    def map(queryPoint: Point): (Point, Vector[Point]) = {
+      val dataSetIn = ExecutionEnvironment.getExecutionEnvironment.fromCollection(pointsIn.toVector)
+      val cluster = new Cluster(dataSetIn, queryPoint)
+      val knn = cluster.kNearestNeighbor(queryPoint, 3)
+      (queryPoint, knn.collect.toVector)
+    }
+  }
+
+
 
 
 
