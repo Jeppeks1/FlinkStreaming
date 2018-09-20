bdacfaf19b648db285b1d2fbbd680564a213ceb7
Jeppeks1 <Jeppeks1@hotmail.com>
2018-09-20 14:19:27 +0200

    Added a working DeCP pipeline for the method using the index for searching.

--- src/main/scala/project/distributed/DeCP.scala
+++ src/main/scala/project/distributed/DeCP.scala
@@ -4,6 +4,8 @@ import org.apache.flink.api.java.functions.FunctionAnnotation.ForwardedFields
 import org.apache.flink.api.common.functions.RichMapFunction
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.common.operators.Order
+import org.apache.flink.util.Collector
 import org.apache.flink.api.scala._
 
 import project.distributed.reader.FeatureVector._
@@ -13,11 +15,11 @@ import project.distributed.container.Cluster
 import project.distributed.container.Point
 
 import scala.collection.JavaConverters._
-import scala.util.Random
+import java.lang.Iterable
 
 object DeCP {
 
-  import org.apache.flink.api.common.functions.{MapFunction, RichFlatMapFunction}
+  import org.apache.flink.api.common.functions.{MapFunction, RichFlatMapFunction, RichGroupReduceFunction}
 
 
   /**
@@ -43,40 +45,83 @@ object DeCP {
     // Get the execution environment and read the data
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
     val queryPoints: DataSet[Point] = readQueryPoints(env, params)
-    val points: DataSet[Point] = readFeatureVector(env, params) // TODO: Broadcast?
+    val qp = queryPoints.first(3)
+    val points1: DataSet[Point] = readFeatureVector(env, params) // TODO: Broadcast?
+    val points = points1.filter(_.pointID > 2)
     val root: InternalNode = buildIndexTree(points, params) // TODO: Broadcast?
 
+    // The set of all points grouped on the cluster they belong to.
+    // Will later be used to retrieve the relevant set of clusters
+    // and perform a kNN search within them.
+    val clusteredPoints = points
+      .map(p => (p, searchIndex(root)(p).pointID))
+      .groupBy(1)
 
     // Process the query points
-    if (method == "scan"){
+    if (method == "scan") {
 
-//      queryPoints.first(4)
-//        .map{new KNNScanRich}
-//        .withBroadcastSet(points, "pointsIn")
-//        .print
-
-
-
-
-
+      //      queryPoints.first(4)
+      //        .map{new KNNScanRich}
+      //        .withBroadcastSet(points, "pointsIn")
+      //        .print
 
 
     }
-    else if (method == "index"){
+    else if (method == "index") {
 
-      val clusteredPoints = points
-        .map(p => (searchIndex(root)(p).toCluster, p))
-        .withForwardedFields("_1->_2") // Input is copied unchanged to position 2 of the output
+      // Discover which clusters to search through.
+      // TODO: Expand this with the parameter b.
+      val query2cluster = qp
+        .map(qp => (qp, searchIndex(root)(qp).pointID))
+
+      query2cluster.print
+      println("Seperator")
+
+      // Search through the relevant clusters.
+      clusteredPoints
+        .reduceGroup(new FindDistances)
+        .withBroadcastSet(query2cluster, "query2cluster")
+        .map(qpID_p => (qpID_p.head._1, qpID_p.head._2.pointID, qpID_p.head._2.distance))
         .groupBy(0)
-
-
-
-
+        .sortGroup(2, Order.ASCENDING)
+        .reduceGroup(new kNearestNeighbors(k))
+        .print
     }
   }
 
+  final class kNearestNeighbors(k: Int) extends RichGroupReduceFunction[(Long, Long, Double), (Long, List[Long], Double)] {
+
+    def reduce(it: Iterable[(Long, Long, Double)],
+               out: Collector[(Long, List[Long], Double)]): Unit = {
+      var group = it.iterator().asScala.toList
+      out.collect((group.head._1, group.map(_._2).take(k), group.head._3))
+    }
+  }
+
+
+  final class FindDistances extends RichGroupReduceFunction[(Point, Long), Traversable[(Long, Point)]] {
+    private var query2cluster: Traversable[(Point, Long)] = _
+
+    override def open(parameters: Configuration): Unit =
+      query2cluster = getRuntimeContext.getBroadcastVariable[(Point, Long)]("query2cluster").asScala
+
+    def reduce(it: Iterable[(Point, Long)],
+               out: Collector[Traversable[(Long, Point)]]): Unit = {
+      it.iterator().asScala.foreach(pl =>
+        // Avoid collecting empty lists
+        if (query2cluster.unzip._2.toVector.contains(pl._2))
+          out.collect(
+            query2cluster.collect { // Filter and distance calculation in one go
+              case ql if ql._2 == pl._2 => (ql._2, pl._1.eucDist(ql._1))
+            }))
+    }
+  }
+
+
+
+
   @ForwardedFields(Array("*->_1"))
-  final class KNearestNeighborRich extends RichMapFunction[Point, (Point, Vector[Point])]{
+  final class KNearestNeighborRich extends RichMapFunction[Point, (Point, Vector[Point])] {
     private var pointsIn: Traversable[Point] = _
 
     override def open(parameters: Configuration): Unit =
@@ -90,11 +135,6 @@ object DeCP {
       (queryPoint, knn.collect.toVector)
     }
   }
-
-
-
-
-
 
 
 }
