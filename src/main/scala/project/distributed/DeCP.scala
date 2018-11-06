3d60b7b41df8a1c255218dabbd681398a7e93585
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-06 20:50:35 +0100

    Added support for a batch implementation of the DeCP method.

--- src/main/scala/project/distributed/DeCP.scala
+++ src/main/scala/project/distributed/DeCP.scala
@@ -1,218 +1,193 @@
 package project.distributed
 
-import org.apache.flink.api.common.functions._
-import org.apache.flink.configuration.Configuration
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.common.operators.Order
+import org.apache.flink.api.common.functions._
 import org.apache.flink.util.Collector
+import org.apache.flink.configuration.Configuration
 import org.apache.flink.api.scala._
+import org.apache.flink.api.scala.createTypeInformation // This explicit import is required for some reason
 
+import org.slf4j.{Logger, LoggerFactory}
+
+import project.distributed.container.{InternalNode, Point}
 import project.distributed.reader.FeatureVector._
-import project.distributed.container.InternalNode
+import project.distributed.reader.PointInputFormat
+import project.distributed.functions._
 import project.distributed.container.IndexTree._
-import project.distributed.container.Point
+import org.apache.flink.core.fs.Path
 
 import scala.collection.JavaConverters._
 
-import java.security.InvalidParameterException
-import java.lang.Iterable
 
+/**
+  * The DeCP object contains the batch implementation of the DeCP pipeline. The implementation uses
+  * the same index construction technique as in the streaming version, but utilizes a different strategy
+  * to connect the points and query points. The set of query points is much smaller than the base points
+  * and is suitable for broadcasting to the downstream operators. The data is connected using the join
+  * operator and compared to the ground truth to obtain an accuracy measure.
+  */
 object DeCP {
+
+  protected val logger: Logger = LoggerFactory.getLogger("StreamingDeCP")
 
   /**
     * Usage:
     * {{{
-    *   DeCP --queryPoints <path>
-    *        --featureVectors <path>
-    *        --groundTruth <path>
-    *        --method <String>
-    *        --L <Int>
-    *        --a <Int>
-    *        --b <Int>
-    *        --k <Int>
+    *   StreamingDeCP --sift <String>
+    *                 --method <String>
+    *                 --recluster <Boolean>
+    *                 --treeA <Int>
+    *                 --L <Int>
+    *                 --a <Int>
+    *                 --b <Int>
+    *                 --k <Int>
     * }}}
     */
   def main(args: Array[String]): Unit = {
-
-    // Initialize input variables
+    // Get the input parameters
     val params: ParameterTool = ParameterTool.fromArgs(args)
-    val featureVectorPath = params.get("featureVectors")
-    val groundTruthPath = params.get("groundTruths")
-    val queryPointPath = params.get("queryPoints")
+
+    // Get and initialize variables from the parameter tool
+    val recluster = params.get("recluster", "false").toBoolean
     val method = params.get("method")
-    val k = params.get("k").toInt
-    val a = params.get("a").toInt
-    val b = params.get("b").toInt
+    val sift = params.get("sift")
+    val L = params.get("L", "4").toInt
+    val a = params.get("a", "1").toInt
+    val b = params.get("b", "1").toInt
+    val k = params.get("k", "5").toInt
+    val treeA = params.get("treeA", "3").toInt
 
-    // Get the execution environment and read the data
+    // Set the paths and configuration properties
+    // val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift + "/" + sift + "_"
+    val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftsmall\\siftsmall_"
+    val featureVectorPath = new Path(siftPath + "base.fvecs")
+    val groundTruthPath = new Path(siftPath + "groundtruth.ivecs")
+    val queryPointPath = new Path(siftPath + "query.fvecs")
+    val clusterPath = new Path(siftPath + "cluster.txt")
+    val indexPath = new Path(siftPath + "index.txt")
+
+    // Get the ExecutionEnvironments and read the data using a PointInputFormat
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
-    val groundTruth: DataSet[Vector[Int]] = ivecs_truth(env, groundTruthPath)
-    val queryPoints: DataSet[Point] = readFeatureVector(env, queryPointPath)
-    val points: DataSet[Point] = readFeatureVector(env, featureVectorPath)
-    val root: InternalNode = buildIndexTree(points, params)
+    val queryPoints: DataSet[Point] = env.createInput(new PointInputFormat(queryPointPath))
+    val points: DataSet[Point] = env.createInput(new PointInputFormat(featureVectorPath))
 
-    // The set of all points grouped on the cluster they belong to.
-    // Will later be used to retrieve the relevant set of clusters
-    // and perform a kNN search within them.
-    val clusteredPoints = points
-      .map(p => (p, searchTheIndex(root, null)(p, a)))
-      .flatMap(new FlatMapper)
-      .groupBy(1)
+    // Read the ground truth and determine the number of points in the input
+    val groundTruth: DataSet[Vector[Int]] = env.fromCollection(ivecs_truth(groundTruthPath.getPath))
+    val inputSize = points.count
 
-    // Process the query points
     val knn = if (method == "scan") {
       // Perform a full sequential scan
-      clusteredPoints
-        .reduceGroup(new SequentialScan)
+      val knn = points
+        .flatMap(new BatchSequentialScan)
         .withBroadcastSet(queryPoints, "queryPoints")
-        .distinct // In case a > 1
-        .groupBy(0)
-        .sortGroup(2, Order.ASCENDING)
-        .reduceGroup(new kNearestNeighbors(k))
-        .setParallelism(1)
-        .sortPartition(0, Order.ASCENDING)
+        .groupBy(_._1) // Group by queryPointID
+        .sortGroup(_._3, Order.ASCENDING) // Sort by distance
+        .first(k) // Select the first k elements in each group
+        .groupBy(_._1) // Re-grouping is unfortunate, but massively reduces the dataflow to the next operator
+        .reduceGroup(new ResultCollector)
 
+      knn
     }
     else if (method == "index") {
-      // Discover which clusters to search through.
-      val query2cluster = queryPoints
+
+      val (root, cluster) = if (recluster) {
+
+        // Find the leaf nodes
+        val leafs = points
+          .filter(new SelectRandomLeafs(inputSize, L)) // TODO: Should not be random
+          .map(p => (1, InternalNode(Vector(), p)))
+
+        // Build the root node
+        val rootNode = leafs.iterate(L - 1) { currentNodes =>
+          // Select new nodes that will make up the nodes of the next level
+          val newNodes = currentNodes
+            .filter(new SelectRandomNodes(inputSize, L))
+            .withBroadcastSet(currentNodes, "currentNodes")
+
+          // For every node in the current level, find the treeA nearest nodes at the next level
+          val parentNodes = currentNodes
+            .map(new FindParents(treeA))
+            .withBroadcastSet(newNodes, "newNodes")
+
+          // Discover the nodes at the previous level, which is nearest to the new node
+          val nodes = newNodes
+            .map(new FindChildren)
+            .withBroadcastSet(parentNodes, "parentNodes")
+
+          nodes
+        }.map(_._2).collect.toVector
+
+        val root = InternalNode(rootNode, rootNode(0).pointNode)
+
+        // Perform the clustering
+        val cp = points
+          .map(p => (p, searchTheIndex(root, null)(p, a)))
+          .flatMap(new FlatMapper)
+
+        // TODO: Write the clustering to a file with a FileOutputFormat
+
+        (root, cp)
+
+      } else {
+        throw new Exception("Not yet implemented: recluster = false")
+      }
+
+      // Discover the clusterID of each query point
+      val qp = queryPoints
         .map(qp => (qp, searchTheIndex(root, null)(qp, b)))
         .flatMap(new FlatMapper)
 
-      // Search through the relevant clusters.
-      clusteredPoints
-        .reduceGroup(new FindDistances)
-        .withBroadcastSet(query2cluster, "query2cluster")
-        .distinct // In case b > 1 and a > 1
-        .groupBy(0)
-        .sortGroup(2, Order.ASCENDING)
-        .reduceGroup(new kNearestNeighbors(k))
-        .setParallelism(1) // Sort globally
-        .sortPartition(0, Order.ASCENDING)
+      // Join the clustered points with the query points and find kNN
+      val knn = cluster
+        .join(qp)
+        .where(1) // clusterID from cluster
+        .equalTo(1) // clusterID from queryPoint
+        .map(in => (in._2._1.pointID, in._1._1.pointID, in._2._1.eucDist(in._1._1))) // (queryPointID, clusterPointID, distance)
+        .distinct // Remove duplicate tuples (relevant if b > 1) TODO: Move this operator up?
+        .groupBy(_._1) // Group by queryPointID
+        .sortGroup(_._3, Order.ASCENDING) // Sort by distance
+        .first(k) // Select the first k elements in each group
+        .groupBy(_._1) // Re-grouping is unfortunate, but first(k) massively reduces the dataflow to the next operator
+        .reduceGroup(new ResultCollector)
 
+      knn
     }
-    else throw new InvalidParameterException("Invalid or missing input parameter --method. " +
+    else throw new Exception("Invalid or missing input parameter --method. " +
       "See documentation for valid options.")
 
-    // Compare the result to the ground truth
-    val compared = knn
-      .map(new compareToGroundTruth(k))
+    val result = knn
+      .map(new GroundTruth(k))
       .withBroadcastSet(groundTruth, "groundTruth")
+      .map(in => (in._1, in._2, in._3))
+      .collect
+      .toVector
 
-    compared.print
-    println("Accuracy: ")
-    compared.setParallelism(1).sum(1).map(x => x._2 / 100).print
+    val hit = result.map(_._2).sum / result.size
+    val count = result.map(_._3).sum / result.size
 
+    println("--- Accuracy ----")
+    println("Hit: " + hit)
+    println("Recall: " + count)
   }
 
 
-  /**
-    * Flattens the incoming (Point, Vector[InternalNode]) records and retrieves the clusterID
-    * from each InternalNode, to be used for grouping. The resulting type is (Point, clusterID).
-    */
-  final class FlatMapper extends FlatMapFunction[(Point, Vector[InternalNode]), (Point, Long)] {
+  final class BatchSequentialScan extends RichFlatMapFunction[Point, (Long, Long, Double)] {
 
-    def flatMap(input: (Point, Vector[InternalNode]),
-                out: Collector[(Point, Long)]): Unit = {
-      input._2.foreach{ in =>
-        out.collect((input._1, in.clusterLeader.pointID))
-      }
-    }
-  }
-
-
-  /**
-    * The SequentialScan class takes the clustered input points on the format (Point, clusterID)
-    * and reduces all the points into a (queryPointID, pointID, distance) record. This process is
-    * repeated O(n * qp) times constituting a full sequential scan.
-    *
-    * @note The clusterID is not used in this class and the original dataset of points could have
-    *       been used instead. As the clustered points are written to disk during pre-processing,
-    *       it is faster to simply read that file, rather than parsing the entire input again.
-    */
-  final class SequentialScan extends RichGroupReduceFunction[(Point, Long), (Long, Long, Double)] {
     private var queryPoints: Traversable[Point] = _
 
-    override def open(parameters: Configuration): Unit =
+    override def open(parameters: Configuration): Unit = {
       queryPoints = getRuntimeContext.getBroadcastVariable[Point]("queryPoints").asScala
+    }
 
-    def reduce(it: Iterable[(Point, Long)],
-               out: Collector[(Long, Long, Double)]): Unit = {
-      it.iterator().asScala.foreach { pl => // For each (Point, clusterID) in the incoming group
-        queryPoints.foreach { qp => // For each query point
-          out.collect(qp.pointID, pl._1.pointID, pl._1.eucDist(qp).distance)
-        }
+    override def flatMap(input: Point, out: Collector[(Long, Long, Double)]): Unit = {
+      // For the incoming point, calculate and emit the distance to all query points
+      queryPoints.foreach { qp =>
+        out.collect((qp.pointID, input.pointID, input.eucDist(qp)))
       }
     }
   }
 
-
-  /**
-    * The FindDistances class takes the clustered input points on the format (Point, clusterID)
-    * and reduces the input to a (queryPointID, pointID, distance) record. The query2cluster
-    * variable contains a mapping from each query point to the cluster leader it is closest to.
-    * This mapping is used to filter irrelevant clusters and compute the relevant distances
-    * in one go.
-    */
-  final class FindDistances extends RichGroupReduceFunction[(Point, Long), (Long, Long, Double)] {
-    private var query2cluster: Traversable[(Point, Long)] = _
-
-    override def open(parameters: Configuration): Unit =
-      query2cluster = getRuntimeContext.getBroadcastVariable[(Point, Long)]("query2cluster").asScala
-
-    def reduce(it: Iterable[(Point, Long)],
-               out: Collector[(Long, Long, Double)]): Unit = {
-      it.iterator().asScala.foreach(pl =>
-        query2cluster.foreach { ql =>
-          if (ql._2 == pl._2) {
-            val point = pl._1.eucDist(ql._1)
-            out.collect((ql._1.pointID, point.pointID, point.distance))
-          }
-        })
-    }
-  }
-
-  /**
-    * Determines the k-nearest neighbors for each incoming (queryPointID, pointID, distance)
-    * record. The resulting output is a record (queryPointID, List[pointID]) containing the
-    * query point ID, a list of the nearest points of size k and the distance from the query
-    * point to the nearest point in the scanned cluster.
-    *
-    * @param k The number of nearest neighbors to return.
-    * @note There is no correctness guarantee for the k-nearest neighbor algorithm and the
-    *       size of the returned list of nearest neighbors may be smaller than k.
-    */
-  final class kNearestNeighbors(k: Int) extends RichGroupReduceFunction[(Long, Long, Double), (Long, List[Long])] {
-
-    def reduce(it: Iterable[(Long, Long, Double)],
-               out: Collector[(Long, List[Long])]): Unit = {
-      val group = it.iterator().asScala.toList
-      out.collect((group.head._1, group.map(_._2).take(k)))
-    }
-  }
-
-
-  /**
-    * Compares a result to the ground truth feature vector in the most convoluted way possible.
-    *
-    * @param k The number of nearest neighbors in the incoming result set.
-    * @note The actual number of returned nearest neighbor might be smaller than k, which is why
-    *       the size of the zipped result/truth vector is used.
-    */
-  final class compareToGroundTruth(k: Int) extends RichMapFunction[(Long, List[Long]), (Long, Double)] {
-    private var groundTruth: Traversable[Vector[Int]] = _
-
-    override def open(parameters: Configuration): Unit =
-      groundTruth = getRuntimeContext.getBroadcastVariable[Vector[Int]]("groundTruth").asScala
-
-    def map(in: (Long, List[Long])): (Long, Double) = {
-      val vecVec = groundTruth.toVector
-      val vec = vecVec(in._1.toInt).take(k)
-      val compare = in._2.take(k).zip(vec)
-      val size = compare.size
-      (in._1, compare.map(li => if (li._1 == li._2.toInt) 1.0 / size else 0.0).sum)
-    }
-  }
-
-
 }
+
+
