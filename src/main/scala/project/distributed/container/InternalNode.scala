3860dcabb85a3737a251d781034f66d35173f007
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-06 20:53:59 +0100

    Added a custom toString method for debugging.

--- src/main/scala/project/distributed/container/InternalNode.scala
+++ src/main/scala/project/distributed/container/InternalNode.scala
@@ -1,23 +1,24 @@
 package project.distributed.container
 
 import java.io.Serializable
-import org.apache.flink.api.scala._
 
-import scala.math.{ceil, floor, pow}
+import scala.math.{ceil, pow}
 import scala.util.Random
 
 /**
   * An InternalNode represents all the nodes of the index tree
   * and is used to guide the incoming points towards the correct leaf.
   *
-  * @param children      The set of nodes that is accessible from this node.
-  * @param clusterLeader The point of the designated cluster leader
-  *                      at the current level.
+  * @param children  The set of nodes that is accessible from this node.
+  * @param pointNode The point of the designated cluster leader
+  *              at the current level.
   */
-case class InternalNode(children: Vector[InternalNode],
-                        clusterLeader: Point) extends Serializable {
+case class InternalNode(var children: Vector[InternalNode],
+                        var pointNode: Point) extends Serializable {
 
   def isLeaf: Boolean = if (children.isEmpty) true else false
+
+  override def toString: String = "[" + pointNode.pointID + "] " + children.map(_.pointNode.pointID)
 }
 
 
@@ -25,7 +26,7 @@ object InternalNode {
 
   var inputSize: Long = _
   var levels: Int = 3
-  val treeA: Int = 3
+  var treeA: Int = _
   val balancingFactor: Double = 0.0
 
 
@@ -54,8 +55,8 @@ object InternalNode {
     }
 
     val vecDistinct = vec.distinct
-    val parents = points.map { p => (p, findParents(vecDistinct)(p.clusterLeader))}
-    vecDistinct.map(p => findChildren(parents)(p.clusterLeader))
+    val parents = points.map{in => (in, findParents(vecDistinct)(in.pointNode))}
+    vecDistinct.map(in => findChildren(parents)(in.pointNode))
   }
 
 
@@ -65,18 +66,17 @@ object InternalNode {
     * @param point A point belonging to a Node at the previous level.
     * @return The treeA closest parent nodes.
     */
-  private def findParents(current: Vector[InternalNode])(point: Point): Vector[Long] = {
+  def findParents(current: Vector[InternalNode])(point: Point): Vector[Long] = {
     current
-      .map(_.clusterLeader.eucDist(point))
-      .sorted
+      .map(in => (in, in.pointNode.eucDist(point)))
+      .sortBy(_._2)
       .slice(0, InternalNode.treeA)
-      .map(_.pointID)
-    // The distance was computed and set for sorting, so we need the pointID for
-    // comparison later and not the Point itself.
+      .map(_._1.pointNode.pointID)
+    // We need the pointID for a comparison in the findChildren method
   }
 
 
-  private def findChildren(parents: Vector[(InternalNode, Vector[Long])])(point: Point): InternalNode = {
+  def findChildren(parents: Vector[(InternalNode, Vector[Long])])(point: Point): InternalNode = {
     val search = parents.collect{
       case x if x._2.contains(point.pointID) => x._1
     }
@@ -84,22 +84,19 @@ object InternalNode {
   }
 
 
-  def pickRandomLeafs(points: DataSet[Point], levelsIn: Int): Vector[InternalNode] = {
-    setVariables(points.count, levelsIn)
+  def pickRandomLeafs(points: Vector[Point], levelsIn: Int): Vector[InternalNode] = {
+    val test = points.size
+    setVariables(test, levelsIn)
     val size = inputSize
     val IOGranularity = 128 * 1024 // 128 KB - based on Linux OS
-    val descriptorSize = points.first(1).collect.head.descriptor.size * 4  // 4 bytes per integer or float
+    val descriptorSize = points.head.descriptor.size * 4  // 4 bytes per integer or float
     //val leaderCount = ceil(size / floor(IOGranularity / descriptorSize)).toInt
     val leaderCount = ceil(pow(size, 1 - 1.0/(levels + 1))).toInt
     val balancedLeaderCount = ceil(leaderCount + leaderCount * balancingFactor).toInt
 
     // Cheating and picking non-random points for simplicity. TODO: Fix
-    points.first(balancedLeaderCount).collect.toVector.map(InternalNode(Vector(), _))
+    points.take(balancedLeaderCount).map(InternalNode(Vector(), _))
   }
 
-
-
-  implicit def orderByDistance[A <: InternalNode]: Ordering[InternalNode] =
-    Ordering.by(_.clusterLeader.distance)
 }
 
