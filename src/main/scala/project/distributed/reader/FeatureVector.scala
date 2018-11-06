7a4a6aaa24380e7aedf55ed60b54039e7bd271f2
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-06 20:40:20 +0100

    Added support for reading the ground truth file and simplified other methods.

--- src/main/scala/project/distributed/reader/FeatureVector.scala
+++ src/main/scala/project/distributed/reader/FeatureVector.scala
@@ -3,11 +3,9 @@ package project.distributed.reader
 import java.io._
 import java.nio.{ByteBuffer, ByteOrder}
 
-import org.apache.flink.api.scala._
-import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.commons.io.FilenameUtils
 import project.distributed.container.Point
-import org.apache.flink.api.common.functions._
+
 
 /**
   * The basic abstraction for reading feature vectors into a DataSet
@@ -21,16 +19,15 @@ object FeatureVector {
     * The dimension of the input vectors must be d = 128 and each vector
     * must have size 4 + 4 * d.
     *
-    * @param env The current execution environment.
-    * @param params ParameterTool containing --featureVector pathToFile
+    * @param path The path to the .ivecs or .fvecs featureVector file.
     * @return A vector representation of all the points in the input file.
     */
-  def readFeatureVector(env: ExecutionEnvironment, params: ParameterTool): DataSet[Point] = {
-    val path = params.get("featureVector")
+  def readFeatureVector(path: String): Vector[Point] = {
+
     val points = FilenameUtils.getExtension(path) match {
-      case "fvecs" => fi_vecs(path, env)
-      case "ivecs" => fi_vecs(path, env)
-      case _ => throw new IOException("Error: Unsupported data input format in readFeatureVector:\n" +
+      case "ivecs" => fi_vecs(path)
+      case "fvecs" => fi_vecs(path)
+      case x => throw new IOException("Error: Unsupported data input format '" + x + "' in readFeatureVector:" +
         "Supported file extensions are .fvecs and .ivecs.")
     }
     points
@@ -38,57 +35,35 @@ object FeatureVector {
 
 
   /**
-    * Method for reading feature vector files with the .ivecs and .fvecs extensions.
+    * Method for reading .ivecs and .fvecs files into a Vector of Points.
     * The integers are stored as floating point numbers for simplicity.
     * The dimension of the input vectors must be d = 128 and each vector
     * must have size 4 + 4 * d.
     *
-    * @param env The current execution environment.
-    * @param params ParameterTool containing --queryPoint pathToFile
-    * @return A vector representation of all the points in the input file.
+    * @param path The path to the feature vector.
+    * @return A Vector representation of all the points in the input file.
     */
-  def readQueryPoints(env: ExecutionEnvironment, params: ParameterTool): DataSet[Point] = {
-    val path = params.get("queryPoint")
-    val points = FilenameUtils.getExtension(path) match {
-      case "fvecs" => fi_vecs(path, env)
-      case "ivecs" => fi_vecs(path, env)
-      case _ => throw new IOException("Error: Unsupported data input format in readFeatureVector:\n" +
-        "Supported file extensions are .fvecs and .ivecs.")
-    }
-    points
-  }
+  private def fi_vecs(path: String): Vector[Point] = {
 
-  /**
-    * Method for reading .ivecs and .fvecs files into a vector of Points.
-    * The integers are stored as floating point numbers for simplicity.
-    * The dimension of the input vectors must be d = 128 and each vector
-    * must have size 4 + 4 * d.
-    * @param filename The path to the feature vector.
-    * @param env The current execution environment.
-    * @return A vector representation of all the points in the input file.
-    */
-  private def fi_vecs(filename: String, env: ExecutionEnvironment): DataSet[Point] = {
-
-    val ext = FilenameUtils.getExtension(filename)
+    val ext = FilenameUtils.getExtension(path)
 
     val data_in = new DataInputStream(
       new BufferedInputStream(
         new FileInputStream(
-          new File(filename))))
+          new File(path))))
 
-    // It appears a DataSet must be non-empty on creation, so we use a placeholder
-    var fvecs: DataSet[Point] = env.fromElements(Point(-1, Vector()))
     val tmpArray = ByteBuffer.allocate(516).array
     val buffer = ByteBuffer.wrap(tmpArray)
     buffer.order(ByteOrder.LITTLE_ENDIAN)
 
     var id = 0
+    var tempVec = Vector[Point]()
     while (data_in.available > 0) {
       data_in.readFully(tmpArray)
       buffer.rewind()
 
       val dim = buffer.getInt
-      if (dim != 128) throw new IOException("Error: Unexpected dimensionality of a feature vector.")
+      if (dim != 128) throw new IOException("Error: Unexpected dimensionality d = " + dim + " of a feature vector.")
 
       var vec = Vector[Float]()
       while (vec.size < dim) {
@@ -97,17 +72,61 @@ object FeatureVector {
         else if (ext == "fvecs")
           vec = vec :+ buffer.getFloat
       }
-      // TODO: Fix stack safety issue
-      fvecs = fvecs.union(env.fromElements(Point(id, vec)))
+      tempVec = tempVec :+ Point(id, vec)
       id = id + 1
     }
 
-    fvecs.filter(_.pointID != -1)
+    data_in.close()
+    tempVec
   }
 
 
+  /**
+    * Method for reading .ivecs groundtruth files into a Vector of Vectors.
+    * The dimension of the input vectors must be d = 100 or d = 1000 and each
+    * vector must have size 4 + 4 * d.
+    *
+    * @param path The path to the ground truth file.
+    * @return A vector representation of all the points in the input file.
+    */
+  def ivecs_truth(path: String): Vector[Vector[Int]] = {
+
+    val ext = FilenameUtils.getExtension(path)
+
+    val data_in = new DataInputStream(
+      new BufferedInputStream(
+        new FileInputStream(
+          new File(path))))
+
+    // TODO: Fix hardcoding of 404 below
+    val tmpArray = ByteBuffer.allocate(404).array
+    val buffer = ByteBuffer.wrap(tmpArray)
+    buffer.order(ByteOrder.LITTLE_ENDIAN)
+
+    var tempVec = Vector[Vector[Int]]()
+    while (data_in.available > 0) {
+      data_in.readFully(tmpArray)
+      buffer.rewind()
+
+      val dim = buffer.getInt
+      if (!(dim == 100 || dim == 1000))
+        throw new IOException("Error: Unexpected dimensionality d = " + dim + " of a ground truth vector.")
+
+      var vec = Vector[Int]()
+      while (vec.size < dim) {
+        vec = vec :+ buffer.getInt
+      }
+      tempVec = tempVec :+ vec
+    }
+
+    data_in.close()
+    tempVec
+  }
 
 }
+
+
+
 
 
 
