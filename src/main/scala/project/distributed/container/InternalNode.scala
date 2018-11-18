68753c4844055a46a97715cf8b4c01683e40b3cf
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-19 00:53:42 +0100

    Moved the searchTheIndex function to the InternalNode object.

--- src/main/scala/project/distributed/container/InternalNode.scala
+++ src/main/scala/project/distributed/container/InternalNode.scala
@@ -1,101 +1,117 @@
 package project.distributed.container
 
-import java.io.Serializable
+import org.apache.flink.core.io.IOReadableWritable
+import org.apache.flink.core.memory.{DataInputView, DataOutputView}
 
 import scala.math.{ceil, pow}
 import scala.util.Random
 
 /**
-  * An InternalNode represents all the nodes of the index tree
-  * and is used to guide the incoming points towards the correct leaf.
+  * The basic structure that represents an index tree. The InternalNode can
+  * be used to efficiently guide the incoming data points to their nearest
+  * cluster, so that only a fraction of all the data points will be considered
+  * at query time, when looking for the k-nearest neighbors of a query point.
   *
   * @param children  The set of nodes that is accessible from this node.
   * @param pointNode The point of the designated cluster leader
-  *              at the current level.
+  *                  at the current level.
   */
-case class InternalNode(var children: Vector[InternalNode],
+case class InternalNode(var children: Array[InternalNode],
                         var pointNode: Point) extends Serializable {
+
+  // Flink requires a default constructor to infer the POJO type and avoid GenericType
+  def this(){
+    this(Array[InternalNode](), new Point(-1, Array[Float]()))
+  }
 
   def isLeaf: Boolean = if (children.isEmpty) true else false
 
-  override def toString: String = "[" + pointNode.pointID + "] " + children.map(_.pointNode.pointID)
+  override def toString: String = "[" + pointNode.pointID + "] " + children.toVector.map(_.pointNode.pointID)
+
+
+
+
+//  override def write(out: DataOutputView): Unit = {
+//    // Write the root pointNode first
+//    out.writeLong(pointNode.pointID)
+//    out.write(pointNode.descriptor.map(_.toByte))
+//
+//    // Write the children recursively
+//    children.foreach{child => writeInformation(out, child, pointNode.pointID)}
+//  }
+//
+//  override def read(in: DataInputView): Unit = {
+//    // Read the root pointNode and the children
+//    pointNode = readPoint(in)
+//    children = readInformation(in, Array())
+//  }
+//
+//  def writeInformation(out: DataOutputView, node: InternalNode, parent: Long): Unit = {
+//    // Write this pointNode
+//    out.writeLong(node.pointNode.pointID)
+//    out.write(node.pointNode.descriptor.map(_.toByte))
+//
+//    node.children.foreach{child => writeInformation(out, child, node.pointNode.pointID)}
+//  }
+//
+//
+//  def readInformation(in: DataInputView, children: Array[InternalNode]): Array[InternalNode] = {
+//    // Read this pointNode
+//    val point = readPoint(in)
+//    val parents = 5
+//    Array(InternalNode(Array(), point))
+//  }
+//
+//  def readPoint(in: DataInputView): Point = {
+//    // Read the serialized pointID
+//    val pointID = in.readLong
+//
+//    // Read the floats into a byte array
+//    val bytes = new Array[Byte](128)
+//    val bytesRead = in.read(bytes)
+//
+//    // Make sure something was read
+//    assert(bytesRead > 0)
+//
+//    // Convert the bytes to floats and re-create the point
+//    val descriptor = bytes.map(_.toFloat)
+//    pointNode = new Point(pointID, descriptor)
+//    pointNode
+//  }
+
 }
 
 
 object InternalNode {
 
-  var inputSize: Long = _
-  var levels: Int = 3
-  var treeA: Int = _
-  val balancingFactor: Double = 0.0
-
-
-  def setVariables(_inputSize: Long, _levels: Int): Unit = {
-    levels = _levels
-    inputSize = _inputSize
-  }
-
-
-  def pickRandomNodes(points: Vector[InternalNode],
-                      level: Int,
-                      seed: Long): Vector[InternalNode] = {
-    val size = points.length
-    var leaderCount = ceil(pow(inputSize, 1 - level.toDouble / (levels.toDouble + 1))).toInt
-
-    // The disrepancy between methods in eCP and DeCP gives rises to cases, where the leader count
-    // at the current level is higher than the count on the level before. Adjust for this case by
-    // setting a default reduction factor of 40 percent.
-    if (leaderCount > size) leaderCount = ceil(size * 0.6).toInt
-
-    val rng = new Random(12)
-    var vec = Vector.fill(leaderCount)(points(rng.nextInt(size.toInt)))
-
-    while (vec.distinct.size != leaderCount) {
-      vec = vec :+ points(rng.nextInt(size.toInt))
-    }
-
-    val vecDistinct = vec.distinct
-    val parents = points.map{in => (in, findParents(vecDistinct)(in.pointNode))}
-    vecDistinct.map(in => findChildren(parents)(in.pointNode))
-  }
-
-
   /**
-    * Finds the treeA closest parent nodes for the incoming point.
-    * @param current Vector of InternalNodes at the current level.
-    * @param point A point belonging to a Node at the previous level.
-    * @return The treeA closest parent nodes.
+    * Searches the index for the cluster leaders that is closest to the
+    * given input point and returns the nearest b points as found by
+    * comparing distances.
+    *
+    * @param current The current InternalNode of the index which can be
+    *                obtained from the method buildIndexTree.
+    * @param previous The previous InternalNode at the level before the current
+    *                 level.
+    * @param point Point used as reference in the search.
+    * @param b The number of nearest clusters to return for extended searches.
+    * @note The search does not always return the optimal result.
+    * @return The nearest cluster leader of the index leafs to the
+    *         input point.
     */
-  def findParents(current: Vector[InternalNode])(point: Point): Vector[Long] = {
-    current
-      .map(in => (in, in.pointNode.eucDist(point)))
-      .sortBy(_._2)
-      .slice(0, InternalNode.treeA)
-      .map(_._1.pointNode.pointID)
-    // We need the pointID for a comparison in the findChildren method
-  }
+  def searchTheIndex(current: InternalNode, previous: InternalNode)(point: Point, b: Int): Array[InternalNode] = {
 
-
-  def findChildren(parents: Vector[(InternalNode, Vector[Long])])(point: Point): InternalNode = {
-    val search = parents.collect{
-      case x if x._2.contains(point.pointID) => x._1
+    if (current.isLeaf){
+      previous.children.map(in =>
+        (in, in.pointNode.eucDist(point))).sortBy(_._2).map(_._1).slice(0, b)
     }
-    InternalNode(search.asInstanceOf[Vector[InternalNode]], point)
-  }
+    else {
+      // Distances are sorted using .minBy
+      val nearestChild = current.children.map(in =>
+        (in, in.pointNode.eucDist(point))).minBy(_._2)._1
 
-
-  def pickRandomLeafs(points: Vector[Point], levelsIn: Int): Vector[InternalNode] = {
-    val test = points.size
-    setVariables(test, levelsIn)
-    val size = inputSize
-    val IOGranularity = 128 * 1024 // 128 KB - based on Linux OS
-    val descriptorSize = points.head.descriptor.size * 4  // 4 bytes per integer or float
-    //val leaderCount = ceil(size / floor(IOGranularity / descriptorSize)).toInt
-    val leaderCount = ceil(pow(size, 1 - 1.0/(levels + 1))).toInt
-    val balancedLeaderCount = ceil(leaderCount + leaderCount * balancingFactor).toInt
-
-    // Cheating and picking non-random points for simplicity. TODO: Fix
-    points.take(balancedLeaderCount).map(InternalNode(Vector(), _))
+      searchTheIndex(nearestChild, current)(point, b)
+    }
   }
 
 }
