4cf7bda170a25a112991f413abc2a3e1b7b93012
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-19 01:52:08 +0100

    Modified to use Arrays and named operators for logging purposes.

--- src/main/scala/project/distributed/StreamingDeCP.scala
+++ src/main/scala/project/distributed/StreamingDeCP.scala
@@ -1,21 +1,19 @@
 package project.distributed
 
-import org.apache.flink.api.java.utils.ParameterTool
-import org.apache.flink.api.common.functions._
-import org.apache.flink.streaming.api.scala._
-import org.apache.flink.util.Collector
-import org.apache.flink.configuration.Configuration
-import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.scala._ // This import must be before the createTypeInformation
+import org.apache.flink.core.fs.FileSystem.WriteMode
+import org.apache.flink.core.fs.Path
 import org.apache.flink.api.scala.createTypeInformation // This explicit import is required for some reason
+import org.apache.flink.api.scala._
+import org.apache.flink.api.java.utils.ParameterTool
 
 import org.slf4j.{Logger, LoggerFactory}
 
-import project.distributed.container.{InternalNode, Point}
-import project.distributed.reader.FeatureVector._
+import project.distributed.container.InternalNode._
+import project.distributed.container.InternalNode
+import project.distributed.container.Point
 import project.distributed.functions._
-import project.distributed.reader.PointInputFormat
-import project.distributed.container.IndexTree._
-import org.apache.flink.core.fs.Path
+import project.distributed.reader._
 
 
 /**
@@ -28,13 +26,11 @@ import org.apache.flink.core.fs.Path
   * which allows the downstream processing operators to read only the intended clusters.
   * At least one IO is required for every query point however.</li>
   *
+  * This file contains the second workaround.
   */
 object StreamingDeCP {
 
-  val logger: Logger = LoggerFactory.getLogger("StreamingDeCP")
-  var clusteredPoints: Vector[(Point, Long)] = _ // Workaround 1 - indexed search
-  var groundTruth: Vector[Vector[Int]] = _
-  var pointsStatic: Vector[Point] = _ // Workaround 1 - sequential scan
+  val log: Logger = LoggerFactory.getLogger("FileStreamingDeCP")
 
   /**
     * Usage:
@@ -54,7 +50,7 @@ object StreamingDeCP {
     val params: ParameterTool = ParameterTool.fromArgs(args)
 
     // Get and initialize variables from the parameter tool
-    val recluster = params.get("recluster", "false").toBoolean
+    val recluster = params.get("recluster", "true").toBoolean
     val method = params.get("method")
     val sift = params.get("sift")
     val L = params.get("L", "4").toInt
@@ -64,47 +60,48 @@ object StreamingDeCP {
     val treeA = params.get("treeA", "3").toInt
 
     // Set the paths and configuration properties
-    // val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift + "/" + sift + "_"
+//     val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift + "/" + sift + "_"
     val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftsmall\\siftsmall_"
     val featureVectorPath = new Path(siftPath + "base.fvecs")
     val groundTruthPath = new Path(siftPath + "groundtruth.ivecs")
     val queryPointPath = new Path(siftPath + "query.fvecs")
-    val clusterPath = new Path(siftPath + "cluster.txt")
-    val indexPath = new Path(siftPath + "index.txt")
+    val clusterPath = new Path(siftPath + "cluster/")
+    val outputPath = new Path(siftPath + "output.txt")
 
     // Get the ExecutionEnvironments and read the data using a PointInputFormat
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
     val streamEnv: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     val queryPoints: DataStream[Point] = streamEnv.readFile(new PointInputFormat(queryPointPath), queryPointPath.toString)
     val points: DataSet[Point] = env.createInput(new PointInputFormat(featureVectorPath))
+    val truth: DataSet[(Int, Array[Int])] = env.createInput(new TruthInputFormat(groundTruthPath))
     val inputSize = points.count
 
-    // Read the ground truth as a normal vector
-    groundTruth = ivecs_truth(groundTruthPath.getPath)
+    // Collect the ground truth so it can be accessed by the streaming method
+    val groundTruth = truth.name("Truth Source").collect.toArray
 
     val knn = if (method == "scan") {
-      // Use the clusteredPoints variable as a placeholder
-      pointsStatic = points.collect().toVector
+      // Collect the points so it can be accessed by the streaming method
+      val pointsStatic = points.collect.toArray
+
+      // Log the start and end time for a throughput metric
+      log.info("Starting sequential scan")
 
       // Perform a full sequential scan
       val knn = queryPoints
-        .map(new StreamingSequentialScan(k))
-        .map(new StreamingGroundTruth(k))
+        .map(new StreamingSequentialScan(pointsStatic, k))
+        .map(new StreamingGroundTruth(groundTruth, k))
+
+      log.info("Finished sequential scan")
 
       knn
     }
     else if (method == "index") {
 
-      val (root, cluster) = if (recluster) {
-
+      val (root, clusteredPoints) = if (recluster) {
         // Find the leaf nodes
-        val leafs = points
-          .filter(new SelectRandomLeafs(inputSize, L)) // TODO: Should not be random
-          .map(p => (1, InternalNode(Vector(), p)))
-
-        val test = leafs.collect.toVector
-        println("Leaf nodes: " + test.map(_._2.pointNode))
-        println("Leaf count: " + test.size)
+        val leafs = points.name("Points Source")
+          .filter(new SelectRandomLeafs(inputSize, L)).name("SelectRandomLeafs") // TODO: Should not be random
+          .map(p => (1, InternalNode(Array(), p))).name("InternalNode Wrapper")
 
         // Build the root node
         val rootNode = leafs.iterate(L - 1) { currentNodes =>
@@ -124,20 +121,16 @@ object StreamingDeCP {
             .withBroadcastSet(parentNodes, "parentNodes")
 
           nodes
-        }.map(_._2).collect.toVector
+        }.map(_._2).name("ReclusterLoop").collect.toArray
 
         val root = InternalNode(rootNode, rootNode(0).pointNode)
 
         // Perform the clustering
         val cp = points
-          .map(p => (p, searchTheIndex(root, null)(p, a)))
-          .flatMap(new FlatMapper)
-
-        // First workaround - will likely perform absurdly bad.
-        // Uncomment this line for experiments with the other workaround.
-        StreamingDeCP.clusteredPoints = cp.collect.toVector
-
-        // TODO: Write the clustering to a file with a FileOutputFormat
+          .map(p => (p, searchTheIndex(root, null)(p, a))).name("SearchTheIndex")
+          .flatMap(new FlatMapper).name("FlatMapper")
+          .collect
+          .toArray
 
         (root, cp)
 
@@ -145,21 +138,29 @@ object StreamingDeCP {
         throw new Exception("Not yet implemented: recluster = false")
       }
 
+      // Log the start and end time for a throughput metric
+      log.info("Starting index search")
+
       // Discover the clusterID of each query point
-      val knn = queryPoints //.filter(_.pointID == 72)
-        .map(qp => (qp, searchTheIndex(root, null)(qp, b)))
-        .flatMap(new KNearestNeighbor(k))
-        .map(new GroundTruth(k))
+      val knn = queryPoints.name("QueryPoints Source")
+        .map(qp => (qp, System.currentTimeMillis)).name("Latency Metric") // Latency metric
+        .map(qp_time => (qp_time._1, qp_time._2, searchTheIndex(root, null)(qp_time._1, b))).name("Index Search")
+        .flatMap(new KNearestNeighbor(clusteredPoints, null, k)).name("KNearestNeighbor")
+        .map(new StreamingGroundTruth(groundTruth, k)).name("StreamingGroundTruth")
+
+      log.info("Finished index search")
 
       knn
     }
     else throw new Exception("Invalid or missing input parameter --method. " +
       "See documentation for valid options.")
 
-    knn.print
+    knn
+      .writeAsCsv(outputPath.getPath, WriteMode.OVERWRITE)
+      .name("WriteAsCsv")
+      .setParallelism(1)
 
-    streamEnv.execute("Streaming Environment")
-
+    streamEnv.execute("Streaming DeCP Environment")
   }
 }
 
