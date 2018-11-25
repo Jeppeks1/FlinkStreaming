76834a3b3035bf6798a8870d6fd3f8816b8573ad
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-25 20:21:15 +0100

    Simplified structure, fixed bugs and major performance issues.

--- src/main/scala/FileStreamingDeCP.scala
+++ src/main/scala/FileStreamingDeCP.scala
@@ -4,11 +4,12 @@ import org.apache.flink.core.fs.Path
 import org.apache.flink.api.scala.createTypeInformation
 import org.apache.flink.api.scala._
 import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.common.io.SerializedOutputFormat
+import org.apache.flink.api.common.io.SerializedInputFormat
 import org.apache.flink.api.common.operators.Order
 
 import org.slf4j.{Logger, LoggerFactory}
 
-import container.InternalNode
 import container.InternalNode._
 import container.Point
 import functions._
@@ -30,13 +31,15 @@ import reader._
 object FileStreamingDeCP {
 
   val log: Logger = LoggerFactory.getLogger("FileStreamingDeCP")
+  val recordSize: Int = 128 * 2 + 8 // 128 chars of two bytes each, eight bytes from the Long pointID
 
   /**
     * Usage:
     * {{{
     *   StreamingDeCP --sift <String>
     *                 --method <String>
-    *                 --recluster <Boolean>
+    *                 --recluster <boolean>
+    *                 --clusterSize <Int>
     *                 --treeA <Int>
     *                 --L <Int>
     *                 --a <Int>
@@ -45,175 +48,152 @@ object FileStreamingDeCP {
     * }}}
     */
   def main(args: Array[String]): Unit = {
-    import java.nio.ByteBuffer
     // Get the input parameters
     val params: ParameterTool = ParameterTool.fromArgs(args)
 
     // Get and initialize variables from the parameter tool
-    val recluster = params.get("recluster", "true").toBoolean
-    val method = params.get("method")
-    val sift = params.get("sift")
-    val L = params.get("L", "4").toInt
+    val recluster = params.getRequired("recluster").toBoolean
+    val method = params.getRequired("method")
+    val sift = params.getRequired("sift")
+    val L = params.get("L", "2").toInt
     val a = params.get("a", "1").toInt
-    val b = params.get("b", "1").toInt
-    val k = params.get("k", "5").toInt
+    val b = params.get("b", "5").toInt
+    val k = params.get("k", "100").toInt
     val treeA = params.get("treeA", "3").toInt
 
     // Set the paths and configuration properties
-    val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift + "/" + sift + "_"
-    // val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftsmall\\siftsmall_"
-    val featureVectorPath = new Path(siftPath + "base.fvecs")
-    val groundTruthPath = new Path(siftPath + "groundtruth.ivecs")
-    val queryPointPath = new Path(siftPath + "query.fvecs")
-    val clusterPath = new Path(siftPath + "cluster/")
-    val outputPath = new Path(siftPath + "output.txt")
+    val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift
+    // val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftmedium\\"
+    val featureVectorPath = new Path(siftPath + "/base.fvecs")
+    val groundTruthPath = new Path(siftPath + "/groundtruth.ivecs")
+    val queryPointPath = new Path(siftPath + "/query.fvecs")
+    val clusterPath = new Path(siftPath + "/cluster/")
+    val outputPath = new Path(siftPath + "/output.csv")
+    val indexPath = new Path(siftPath + "/cluster/indexLeafs")
 
     // Get the ExecutionEnvironments and read the data using a PointInputFormat
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
     val streamEnv: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
     val queryPoints: DataStream[Point] = streamEnv.readFile(new PointInputFormat(queryPointPath), queryPointPath.toString)
     val points: DataSet[Point] = env.createInput(new PointInputFormat(featureVectorPath)).name("Point Source")
-    val truth: DataSet[(Int, Array[Int])] = env.createInput(new TruthInputFormat(groundTruthPath))
-    val inputSize = points.count
+    val truth: DataSet[(Int, Array[Int])] = env.createInput(new TruthInputFormat(groundTruthPath)).setParallelism(4)
 
-    // Read the groundTruth vectors
+    // Collect the ground truth so it can be accessed by the streaming method
     val groundTruth = truth.name("Truth Source").collect.toArray
-
-    // Check if the clustered files from previous tests were not deleted
-    val hdfsPath = new org.apache.hadoop.fs.Path(clusterPath.toString)
-    val hdfsConfig = new org.apache.hadoop.conf.Configuration()
-    val fileSystem = hdfsPath.getFileSystem(hdfsConfig)
-    if (!fileSystem.listStatus(hdfsPath).isEmpty)
-      throw new Exception("Error: ClusterPath is not empty before performing clustering.")
 
     // Perform the kNN scan or search
     val knn = if (method == "scan") {
-      // Determine the number of clusters to write the points to
-      val clusterSize = expectedIndexSize(inputSize, 1, L)
 
-      // Measure the time it takes to cluster the points
-      val clusterStart = System.currentTimeMillis
+      if (recluster){
+        // Set the number of clusters to write the points to
+        val clusterSize = params.getRequired("clusterSize").toInt
 
-      // Distribute the points randomly to clusterSize clusters
-      points.map(p => (p, math.ceil(math.random * clusterSize).toLong))
-        .partitionByHash(_._2).setParallelism(1).name("HashPartition")
-        .sortPartition(_._2, Order.ASCENDING).setParallelism(1).name("SortPartition")
-        .write(new ClusterOutputFormat(clusterPath), clusterPath.toString, WriteMode.NO_OVERWRITE).name("SinkToCluster")
-        .setParallelism(1)
+        // Assert the target path is empty before reclustering
+        checkTargetPath(clusterPath)
 
-      // Write to the log
-      log.info("Clustering the points to a file finished in " + (System.currentTimeMillis - clusterStart) + " milliseconds")
+        // Measure the time it takes to complete the scan
+        val clusterStart = System.currentTimeMillis
 
-      // Force the execution of the clustering, so that the files are written when the next section is reached
-      env.execute("FileStreaming DeCP - Clustering")
+        // Distribute the points randomly to clusterSize clusters
+        points.map(p => (p, math.ceil(math.random * clusterSize).toLong)).name("RandomCluster")
+          .sortPartition(_._2, Order.ASCENDING).setParallelism(1).name("SortPartition")
+          .write(new ClusterOutputFormat(clusterPath), clusterPath.toString, WriteMode.NO_OVERWRITE).name("SinkToCluster")
+          .setParallelism(1)
 
-      // Measure the time it takes to complete the scan
-      val scanStart = System.currentTimeMillis
+        // Force the execution of the clustering, so that the throughput metric is accurate
+        env.execute("FileStreaming DeCP - Clustering")
 
-      // Perform a sequential scan by reading one clusterID at a time
-      val knn = queryPoints.name("QueryPoints Source")
+        // Write to the log
+        log.info("Clustering the points finished in " + (System.currentTimeMillis - clusterStart) + " milliseconds")
+      }
+
+      // Rebalance the incoming Points and perform a sequential scan by reading one clusterID at a time
+      val knn = queryPoints.name("QueryPoints Source").rebalance
         .map(new FileStreamingSequentialScan(clusterPath, k)).name("FileStreamingSequentialScan")
-        .map(new StreamingGroundTruth(groundTruth, k)).name("StreamingGroundTruth") //TODO: Sink before this to check
+        .map(new StreamingGroundTruth(groundTruth, k)).name("StreamingGroundTruth")
 
-      // Write to the log
-      log.info("File-based sequential scan finished in " + (System.currentTimeMillis - scanStart) + " milliseconds")
+      // Forcing the streamEnv here and again at the writeAsCsv method is not an option,
+      // as the above code will be executed twice. The writeAsCsv method is fast enough,
+      // that I include the time it takes to write the output in the metric that measures
+      // the search or scan time.
 
       knn
     }
     else if (method == "index") {
-      // Build the root node of the index
-      val root = buildTheIndex(points, inputSize, treeA, L)
 
-      // Measure the time it takes to cluster the points
-      val clusterStart = System.currentTimeMillis
+      val leafs = if (recluster){
+        // Assert the target path is empty before reclustering
+        checkTargetPath(clusterPath)
 
-      // Perform the clustering and write them to a file based on the clusterID
-      points.map(p => (p, searchTheIndex(root, null)(p, a))).name("SearchTheIndex")
-        .flatMap(new FlatMapper).name("FlatMapper")
-        .sortPartition(_._2, Order.ASCENDING).setParallelism(1).name("SortPartition")
-        .write(new ClusterOutputFormat(clusterPath), clusterPath.toString, WriteMode.NO_OVERWRITE).name("SinkToCluster")
-        .setParallelism(1)
+        // Build the root node of the index for the index search and get the leafs
+        val root = buildTheIndex(points, recordSize, treeA, L)
+        val leafs = getLeafs(root, L)
 
-      // Write to the log
-      log.info("Clustering the points to a file finished in " + (System.currentTimeMillis - clusterStart) + " milliseconds")
+        // Measure the time it takes to complete the scan
+        val clusterStart = System.currentTimeMillis
 
-      // Force the execution of the clustering, so that the files are written when the next section is reached
-      env.execute("FileStreaming DeCP - Clustering")
+        // Perform the clustering and write them to a file based on the clusterID
+        points.map(p => (p, searchTheIndex(root, null)(p, a))).name("SearchTheIndex")
+          .flatMap(new FlatMapper).name("FlatMapper")
+          .sortPartition(_._2, Order.ASCENDING).setParallelism(1).name("SortPartition")
+          .write(new ClusterOutputFormat(clusterPath), clusterPath.toString, WriteMode.NO_OVERWRITE).name("SinkToCluster")
+          .setParallelism(1)
 
-      // Measure the time it takes to complete the index search
-      val searchStart = System.currentTimeMillis
+        // Write the leafs to a file to possibly be retrieved later
+        env.fromCollection(leafs)
+          .write(new SerializedOutputFormat[Point], indexPath.toString, WriteMode.NO_OVERWRITE)
+          .setParallelism(1)
 
-      // Discover the clusterID of each query point
-      val knn = queryPoints.name("QueryPoints Source")
-        .map(qp => (qp, System.currentTimeMillis)).name("Latency Metric")
-        .map(qp_time => (qp_time._1, qp_time._2, searchTheIndex(root, null)(qp_time._1, b))).name("Index Search")
-        .flatMap(new KNearestNeighbor(null, clusterPath, k)).name("KNearestNeighbor")
+        // Force the execution of the clustering, so that the throughput metric is accurate
+        env.execute("FileStreaming DeCP - Clustering")
+
+        // Write to the log
+        log.info("Clustering the points finished in " + (System.currentTimeMillis - clusterStart) + " milliseconds")
+
+        leafs
+      } else {
+        // Read the leaf points that was previously written
+        env.readFile(new SerializedInputFormat[Point], indexPath.toString).collect.toArray
+      }
+
+      // Rebalance the incoming Points to every downstream map slot and perform the index search.
+      val knn = queryPoints.name("QueryPoints Source").rebalance
+        .map(new KNearestNeighbor(null, leafs, clusterPath, b, k)).name("KNearestNeighbor")
         .map(new StreamingGroundTruth(groundTruth, k)).name("StreamingGroundTruth")
 
-      // Write to the log
-      log.info("File-based index search finished in " + (System.currentTimeMillis - searchStart) + " milliseconds")
+      // Forcing the streamEnv here and again at the writeAsCsv method is not an option,
+      // as the above code will be executed twice. The writeAsCsv method is fast enough,
+      // that I include the time it takes to write the output in the metric that measures
+      // the search or scan time.
 
       knn
     }
     else throw new Exception("Invalid or missing input parameter --method. " +
       "See documentation for valid options.")
 
-    knn.writeAsCsv(outputPath.getPath, WriteMode.OVERWRITE)
+    // Write the output and replace the decimal separator with a comma for easier post-processing.
+    knn.writeAsCsv(outputPath.getPath, WriteMode.OVERWRITE, "\n", ";")
       .name("WriteAsCsv")
       .setParallelism(1)
 
+    // Force the final output to be written
     streamEnv.execute("FileStreaming DeCP - kNN")
-
   }
+
 
   /**
-    * Builds the index based on the `points` with height `L` and `treeA` internal
-    * connections between child and parent nodes.
+    * Checks if the given path contains any files. The file-based streaming method depends
+    * on written files and must not see stale files when reclustering.
     *
-    * @param points    DataSet containing the base Points.
-    * @param inputSize The number of Points in `points`.
-    * @param treeA     The number of parent nodes to connect each node at the current
-    *                  level to.
-    * @param L         The height of the index.
-    * @return The root node of the index.
+    * @param clusterPath The path to the clustering directory.
     */
-  def buildTheIndex(points: DataSet[Point], inputSize: Long, treeA: Int, L: Int): InternalNode = {
-    // Measure the time it takes to build the index
-    val rootStart = System.currentTimeMillis
-
-    // Find the leaf nodes
-    val leafs = points
-      .filter(new SelectRandomLeafs(inputSize, L)).name("SelectRandomLeafs")
-      .map(p => (1, InternalNode(Array(), p))).name("InternalNode Wrapper")
-
-    // Build the root node
-    val rootNode = leafs.iterate(L - 1) { currentNodes =>
-      // Select new nodes that will make up the nodes of the next level
-      val newNodes = currentNodes
-        .filter(new SelectRandomNodes(inputSize, L)).name("SelectRandomNodes")
-        .withBroadcastSet(currentNodes, "currentNodes")
-
-      // For every node in the current level, find the treeA nearest nodes at the next level
-      val parentNodes = currentNodes
-        .map(new FindParents(treeA)).name("FindParents")
-        .withBroadcastSet(newNodes, "newNodes")
-
-      // Discover the nodes at the previous level, which is nearest to the new node
-      val nodes = newNodes
-        .map(new FindChildren).name("FindChildren")
-        .withBroadcastSet(parentNodes, "parentNodes")
-
-      nodes
-    }.map(_._2).name("Recluster Loop").collect.toArray
-
-    val root = InternalNode(rootNode, rootNode(0).pointNode)
-
-    log.info("Finished building the index in " + (System.currentTimeMillis - rootStart) + " milliseconds")
-    for (level <- 1 to L)
-      log.info("The size of the index at level " + level + " is " + actualIndexSize(root, level, L) + "/" + expectedIndexSize(inputSize, level, L))
-
-    root
+  def checkTargetPath(clusterPath: Path): Unit = {
+    // Check if the clustered files from previous tests were not deleted
+    val hdfsPath = new org.apache.hadoop.fs.Path(clusterPath.toString)
+    val hdfsConfig = new org.apache.hadoop.conf.Configuration()
+    val fileSystem = hdfsPath.getFileSystem(hdfsConfig)
+    if (!fileSystem.listStatus(hdfsPath).isEmpty)
+      throw new Exception("Error: ClusterPath is not empty before performing clustering.")
   }
-
 }
 
