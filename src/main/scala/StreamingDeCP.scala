35bbb69697780f2b159bbccb58e0751d77d3b0bb
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-25 21:42:13 +0100

    Updated the structure of the memory-based streaming method.

--- src/main/scala/StreamingDeCP.scala
+++ src/main/scala/StreamingDeCP.scala
@@ -8,7 +8,7 @@ import org.apache.flink.api.java.utils.ParameterTool
 import org.slf4j.{Logger, LoggerFactory}
 
 import container.InternalNode._
-import container.{InternalNode, Point}
+import container.Point
 import functions._
 import reader._
 
@@ -27,14 +27,14 @@ import reader._
   */
 object StreamingDeCP {
 
-  val log: Logger = LoggerFactory.getLogger("FileStreamingDeCP")
+  val log: Logger = LoggerFactory.getLogger("StreamingDeCP")
+  val recordSize: Int = 128 * 4 + 4 // 128 floats of four bytes each, 4 bytes from the Long pointID
 
   /**
     * Usage:
     * {{{
     *   StreamingDeCP --sift <String>
     *                 --method <String>
-    *                 --recluster <Boolean>
     *                 --treeA <Int>
     *                 --L <Int>
     *                 --a <Int>
@@ -47,23 +47,22 @@ object StreamingDeCP {
     val params: ParameterTool = ParameterTool.fromArgs(args)
 
     // Get and initialize variables from the parameter tool
-    val recluster = params.get("recluster", "true").toBoolean
-    val method = params.get("method")
-    val sift = params.get("sift")
-    val L = params.get("L", "4").toInt
+    val method = params.getRequired("method")
+    val sift = params.getRequired("sift")
+    val L = params.get("L", "2").toInt
     val a = params.get("a", "1").toInt
-    val b = params.get("b", "1").toInt
-    val k = params.get("k", "5").toInt
+    val b = params.get("b", "5").toInt
+    val k = params.get("k", "100").toInt
     val treeA = params.get("treeA", "3").toInt
 
     // Set the paths and configuration properties
-//     val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift + "/" + sift + "_"
-    val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftsmall\\siftsmall_"
-    val featureVectorPath = new Path(siftPath + "base.fvecs")
-    val groundTruthPath = new Path(siftPath + "groundtruth.ivecs")
-    val queryPointPath = new Path(siftPath + "query.fvecs")
-    val clusterPath = new Path(siftPath + "cluster/")
-    val outputPath = new Path(siftPath + "output.txt")
+    val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift
+    // val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftmedium\\"
+    val featureVectorPath = new Path(siftPath + "/base.fvecs")
+    val groundTruthPath = new Path(siftPath + "/groundtruth.ivecs")
+    val queryPointPath = new Path(siftPath + "/query.fvecs")
+    val clusterPath = new Path(siftPath + "/cluster/")
+    val outputPath = new Path(siftPath + "/output.csv")
 
     // Get the ExecutionEnvironments and read the data using a PointInputFormat
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
@@ -71,7 +70,6 @@ object StreamingDeCP {
     val queryPoints: DataStream[Point] = streamEnv.readFile(new PointInputFormat(queryPointPath), queryPointPath.toString)
     val points: DataSet[Point] = env.createInput(new PointInputFormat(featureVectorPath))
     val truth: DataSet[(Int, Array[Int])] = env.createInput(new TruthInputFormat(groundTruthPath))
-    val inputSize = points.count
 
     // Collect the ground truth so it can be accessed by the streaming method
     val groundTruth = truth.name("Truth Source").collect.toArray
@@ -80,84 +78,58 @@ object StreamingDeCP {
       // Collect the points so it can be accessed by the streaming method
       val pointsStatic = points.collect.toArray
 
-      // Log the start and end time for a throughput metric
-      log.info("Starting sequential scan")
-
       // Perform a full sequential scan
-      val knn = queryPoints
-        .map(new StreamingSequentialScan(pointsStatic, k))
-        .map(new StreamingGroundTruth(groundTruth, k))
+      val knn = queryPoints.name("QueryPoints Source").rebalance
+        .map(new StreamingSequentialScan(pointsStatic, k)).name("FileStreamingSequentialScan")
+        .map(new StreamingGroundTruth(groundTruth, k)).name("StreamingGroundTruth")
 
-      log.info("Finished sequential scan")
+      // Forcing the streamEnv here and again at the writeAsCsv method is not an option,
+      // as the above code will be executed twice. The writeAsCsv method is fast enough,
+      // that I include the time it takes to write the output in the metric that measures
+      // the search or scan time.
 
       knn
     }
     else if (method == "index") {
+      // Build the root node of the index for the index search and get the leafs
+      val root = buildTheIndex(points, recordSize, treeA, L)
+      val leafs = getLeafs(root, L)
 
-      val (root, clusteredPoints) = if (recluster) {
-        // Find the leaf nodes
-        val leafs = points.name("Points Source")
-          .filter(new SelectRandomLeafs(inputSize, L)).name("SelectRandomLeafs") // TODO: Should not be random
-          .map(p => (1, InternalNode(Array(), p))).name("InternalNode Wrapper")
+      // Measure the time it takes to cluster the points
+      val clusterStart = System.currentTimeMillis
 
-        // Build the root node
-        val rootNode = leafs.iterate(L - 1) { currentNodes =>
-          // Select new nodes that will make up the nodes of the next level
-          val newNodes = currentNodes
-            .filter(new SelectRandomNodes(inputSize, L))
-            .withBroadcastSet(currentNodes, "currentNodes")
+      // Perform the clustering
+      val clusteredPoints = points
+        .map(p => (p, searchTheIndex(root, null)(p, a))).name("SearchTheIndex")
+        .flatMap(new FlatMapper).name("FlatMapper")
+        .collect
+        .toArray
 
-          // For every node in the current level, find the treeA nearest nodes at the next level
-          val parentNodes = currentNodes
-            .map(new FindParents(treeA))
-            .withBroadcastSet(newNodes, "newNodes")
+      // Write to the log
+      log.info("Clustering the points finished in " + (System.currentTimeMillis - clusterStart) + " milliseconds")
 
-          // Discover the nodes at the previous level, which is nearest to the new node
-          val nodes = newNodes
-            .map(new FindChildren)
-            .withBroadcastSet(parentNodes, "parentNodes")
-
-          nodes
-        }.map(_._2).name("ReclusterLoop").collect.toArray
-
-        val root = InternalNode(rootNode, rootNode(0).pointNode)
-
-        // Perform the clustering
-        val cp = points
-          .map(p => (p, searchTheIndex(root, null)(p, a))).name("SearchTheIndex")
-          .flatMap(new FlatMapper).name("FlatMapper")
-          .collect
-          .toArray
-
-        (root, cp)
-
-      } else {
-        throw new Exception("Not yet implemented: recluster = false")
-      }
-
-      // Log the start and end time for a throughput metric
-      log.info("Starting index search")
-
-      // Discover the clusterID of each query point
-      val knn = queryPoints.name("QueryPoints Source")
-        .map(qp => (qp, System.currentTimeMillis)).name("Latency Metric") // Latency metric
-        .map(qp_time => (qp_time._1, qp_time._2, searchTheIndex(root, null)(qp_time._1, b))).name("Index Search")
-        .flatMap(new KNearestNeighbor(clusteredPoints, null, k)).name("KNearestNeighbor")
+      // Rebalance the incoming Points to every downstream map slot and perform the index search.
+      val knn = queryPoints.name("QueryPoints Source").rebalance
+        .map(new KNearestNeighbor(clusteredPoints, leafs, clusterPath, b, k)).name("KNearestNeighbor")
         .map(new StreamingGroundTruth(groundTruth, k)).name("StreamingGroundTruth")
 
-      log.info("Finished index search")
+      // Forcing the streamEnv here and again at the writeAsCsv method is not an option,
+      // as the above code will be executed twice. The writeAsCsv method is fast enough,
+      // that I include the time it takes to write the output in the metric that measures
+      // the search or scan time.
 
       knn
     }
     else throw new Exception("Invalid or missing input parameter --method. " +
       "See documentation for valid options.")
 
-    knn
-      .writeAsCsv(outputPath.getPath, WriteMode.OVERWRITE)
+    // Write the output and replace the decimal separator with a comma for easier post-processing.
+    knn.writeAsCsv(outputPath.getPath, WriteMode.OVERWRITE, "\n", ";")
       .name("WriteAsCsv")
       .setParallelism(1)
 
-    streamEnv.execute("Streaming DeCP Environment")
+    // Force the final output to be written
+    streamEnv.execute("FileStreaming DeCP - kNN")
   }
 }
 
