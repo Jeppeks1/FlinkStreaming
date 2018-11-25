6055234fc841375bdec369d1f0654fa659adde4a
Jeppeks1 <Jeppeks1@hotmail.com>
2018-11-25 21:41:38 +0100

    Updated the structure and metrics of the batch-based method.

--- src/main/scala/DeCP.scala
+++ src/main/scala/DeCP.scala
@@ -2,9 +2,12 @@ import org.apache.flink.api.common.operators.Order
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala.{createTypeInformation, _}
 import org.apache.flink.core.fs.Path
+import org.apache.flink.util.Collector
+
 import org.slf4j.{Logger, LoggerFactory}
+
 import container.InternalNode._
-import container.{InternalNode, Point}
+import container.Point
 import functions._
 import reader._
 
@@ -18,7 +21,8 @@ import reader._
   */
 object DeCP {
 
-  protected val logger: Logger = LoggerFactory.getLogger("StreamingDeCP")
+  private val log: Logger = LoggerFactory.getLogger("DeCP")
+  private val recordSize: Int = 128 * 4 + 4 // 128 floats of four bytes each, 4 bytes from the Long pointID
 
   /**
     * Usage:
@@ -34,41 +38,36 @@ object DeCP {
     * }}}
     */
   def main(args: Array[String]): Unit = {
+    import org.apache.flink.core.fs.FileSystem.WriteMode
     // Get the input parameters
     val params: ParameterTool = ParameterTool.fromArgs(args)
 
     // Get and initialize variables from the parameter tool
-    val recluster = params.get("recluster", "false").toBoolean
-    val method = params.get("method")
-    val sift = params.get("sift")
-    val L = params.get("L", "4").toInt
+    val method = params.getRequired("method")
+    val sift = params.getRequired("sift")
+    val L = params.get("L", "2").toInt
     val a = params.get("a", "1").toInt
-    val b = params.get("b", "1").toInt
-    val k = params.get("k", "5").toInt
+    val b = params.get("b", "5").toInt
+    val k = params.get("k", "100").toInt
     val treeA = params.get("treeA", "3").toInt
 
     // Set the paths and configuration properties
-     val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift + "/" + sift + "_"
-//    val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftsmall\\siftsmall_"
-    val featureVectorPath = new Path(siftPath + "base.fvecs")
-    val groundTruthPath = new Path(siftPath + "groundtruth.ivecs")
-    val queryPointPath = new Path(siftPath + "query.fvecs")
-    val clusterPath = new Path(siftPath + "cluster/")
-    val indexPath = new Path(siftPath + "index.txt")
+//    val siftPath = "hdfs://h1.itu.dk:8020/user/jeks/data/" + sift
+    val siftPath = "file:\\C:\\Users\\Jeppe-Pc\\Documents\\Universitet\\IntelliJ\\Flink\\data\\siftsmall\\"
+    val featureVectorPath = new Path(siftPath + "/base.fvecs")
+    val groundTruthPath = new Path(siftPath + "/groundtruth.ivecs")
+    val queryPointPath = new Path(siftPath + "/query.fvecs")
+    val outputPath = new Path(siftPath + "/output.csv")
 
-    // Get the ExecutionEnvironments and read the data using a PointInputFormat
+    // Get the ExecutionEnvironments and read the data using InputFormats
     val env: ExecutionEnvironment = ExecutionEnvironment.getExecutionEnvironment
+    val groundTruth: DataSet[(Int, Array[Int])] = env.createInput(new TruthInputFormat(groundTruthPath)).setParallelism(4)
     val queryPoints: DataSet[Point] = env.createInput(new PointInputFormat(queryPointPath))
     val points: DataSet[Point] = env.createInput(new PointInputFormat(featureVectorPath))
 
-    // Read the ground truth and determine the number of points in the input
-    val groundTruth: DataSet[(Int, Array[Int])] = env.createInput(new TruthInputFormat(groundTruthPath))
-    val inputSize = points.count
-
     val knn = if (method == "scan") {
       // Perform a full sequential scan
-      val knn = points
-        .flatMap(new BatchSequentialScan)
+      val knn = points.flatMap(new BatchSequentialScan)
         .withBroadcastSet(queryPoints, "queryPoints")
         .groupBy(_._1) // Group by queryPointID
         .sortGroup(_._3, Order.ASCENDING) // Sort by distance
@@ -79,57 +78,27 @@ object DeCP {
       knn
     }
     else if (method == "index") {
+      // Build the root node of the index for the index search and get the leafs
+      val root = buildTheIndex(points, recordSize, treeA, L)
+      val leafs = getLeafs(root, L)
 
-      val (root, cluster) = if (recluster) {
-
-        // Find the leaf nodes
-        val leafs = points
-          .filter(new SelectRandomLeafs(inputSize, L)) // TODO: Should not be random
-          .map(p => (1, InternalNode(Array(), p)))
-
-        // Build the root node
-        val rootNode = leafs.iterate(L - 1) { currentNodes =>
-          // Select new nodes that will make up the nodes of the next level
-          val newNodes = currentNodes
-            .filter(new SelectRandomNodes(inputSize, L))
-            .withBroadcastSet(currentNodes, "currentNodes")
-
-          // For every node in the current level, find the treeA nearest nodes at the next level
-          val parentNodes = currentNodes
-            .map(new FindParents(treeA))
-            .withBroadcastSet(newNodes, "newNodes")
-
-          // Discover the nodes at the previous level, which is nearest to the new node
-          val nodes = newNodes
-            .map(new FindChildren)
-            .withBroadcastSet(parentNodes, "parentNodes")
-
-          nodes
-        }.map(_._2).collect.toArray
-
-        val root = InternalNode(rootNode, rootNode(0).pointNode)
-
-        // Perform the clustering
-        val cp = points
-          .map(p => (p, searchTheIndex(root, null)(p, a)))
-          .flatMap(new FlatMapper)
-
-        (root, cp)
-
-      } else {
-        throw new Exception("Not yet implemented: recluster = false")
-      }
+      // Perform the clustering
+      val cluster = points
+        .map(p => (p, searchTheIndex(root, null)(p, a)))
+        .flatMap(new FlatMapper)
 
       // Discover the clusterID of each query point
-      val qp = queryPoints
-        .map(qp => (qp, searchTheIndex(root, null)(qp, b)))
-        .flatMap(new FlatMapper)
+      val flattenedQueryPoints = queryPoints
+        .flatMap{(p, col: Collector[(Point, Long)]) =>
+          val slice = leafs.map(l => (l.pointID, l.eucDist(p))).sortBy(_._2).slice(0, b)
+          slice.foreach( in => col.collect(p, in._1))
+        }
 
       // Join the clustered points with the query points and find kNN
       val knn = cluster
-        .join(qp)
-        .where(1) // clusterID from cluster
-        .equalTo(1) // clusterID from queryPoint
+        .join(flattenedQueryPoints)
+        .where(_._2) // clusterID from cluster
+        .equalTo(_._2) // clusterID from flattenedQueryPoints
         .map(in => (in._2._1.pointID, in._1._1.pointID, in._2._1.eucDist(in._1._1))) // (queryPointID, clusterPointID, distance)
         .distinct // Remove duplicate tuples (relevant if b > 1) TODO: Move this operator up?
         .groupBy(_._1) // Group by queryPointID
@@ -143,22 +112,12 @@ object DeCP {
     else throw new Exception("Invalid or missing input parameter --method. " +
       "See documentation for valid options.")
 
-    val result = knn
-      .map(new GroundTruth(k))
+    knn.map(new GroundTruth(k))
       .withBroadcastSet(groundTruth, "groundTruth")
-      .map(in => (in._1, in._2, in._3))
-      .collect
-      .toVector
-
-    val hit = result.map(_._2).sum / result.size
-    val count = result.map(_._3).sum / result.size
-
-    println("--- Accuracy ----")
-    println("Hit: " + hit)
-    println("Recall: " + count)
+      .writeAsCsv(outputPath.getPath, "\n", ";", WriteMode.OVERWRITE)
+      .name("WriteAsCsv")
+      .setParallelism(1)
   }
-
-
 
 }
 
